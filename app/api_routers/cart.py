16181e47f8ee0c360d90e9c872d3f9b714167681
Roger Bif <rbif03@gmail.com>
2026-07-16 14:54:45 -0300

    add auth to cart routes

--- app/api_routers/cart.py
+++ app/api_routers/cart.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from psycopg import Connection
 
+from auth.auth import get_user_from_token
 from db.connect import get_db_conn
 from db.exceptions import (
     DatabaseError,
@@ -30,18 +31,19 @@ from models.cart_item import (
 )
 from models.order import OrderExtended
 from models.order_item import OrderItemCreate
+from models.user import UserReadInternal
 
 router = APIRouter()
 
 
-@router.post("/{user_id}", status_code=201)
+@router.post("/", status_code=201)
 def add_item_to_cart(
     body: CartItemCreateRequest,
-    user_id: int,
+    user: UserReadInternal = Depends(get_user_from_token),
     db: Connection = Depends(get_db_conn),
 ) -> CartItemRead:
 
-    cart_item_data = CartItemCreate(user_id=user_id, **body.model_dump())
+    cart_item_data = CartItemCreate(user_id=user.id, **body.model_dump())
     try:
         result = insert_item_to_cart(db, cart_item_data)
         return result
@@ -65,17 +67,17 @@ def add_item_to_cart(
         )
 
 
-@router.patch("/{user_id}", status_code=200)
+@router.patch("/", status_code=200)
 def patch_cart_item(
     body: CartItemUpdateRequest,
-    user_id: int,
+    user: UserReadInternal = Depends(get_user_from_token),
     db: Connection = Depends(get_db_conn),
 ) -> CartItemRead:
     update_data = CartItemUpdate(**body.model_dump())
     item_id = body.item_id
 
     try:
-        result = update_cart_item(db, user_id, item_id, update_data)
+        result = update_cart_item(db, user.id, item_id, update_data)
         return result
 
     except ItemNotInCartError:
@@ -91,21 +93,24 @@ def patch_cart_item(
         )
 
 
-@router.post("/checkout/{user_id}")
-def checkout_cart(user_id: int, db: Connection = Depends(get_db_conn)) -> OrderExtended:
+@router.post("/checkout/")
+def checkout_cart(
+    user: UserReadInternal = Depends(get_user_from_token),
+    db: Connection = Depends(get_db_conn),
+) -> OrderExtended:
     try:
-        cart_items = get_cart_items(db, user_id)
+        cart_items = get_cart_items(db, user.id)
         if not cart_items:
             raise EmptyCartError
 
-        order = create_new_pending_order(db, user_id)
+        order = create_new_pending_order(db, user.id)
         order_items = [
             OrderItemCreate(order_id=order.id, item_id=item.item_id, amount=item.amount)
             for item in cart_items
         ]
 
         new_order_items = attribute_items_to_order(db, order_items)
-        clear_cart(db, user_id)
+        clear_cart(db, user.id)
         db.commit()
         return OrderExtended(order=order, order_items=new_order_items)
 
@@ -125,12 +130,13 @@ def checkout_cart(user_id: int, db: Connection = Depends(get_db_conn)) -> OrderE
         )
 
 
-@router.get("/{user_id}", status_code=200)
+@router.get("/", status_code=200)
 def list_cart_items(
-    user_id: int, db: Connection = Depends(get_db_conn)
+    user: UserReadInternal = Depends(get_user_from_token),
+    db: Connection = Depends(get_db_conn),
 ) -> List[CartItemReadWithItem]:
     try:
-        result = get_cart_items_with_item_data(db, user_id)
+        result = get_cart_items_with_item_data(db, user.id)
         return result
 
     except DatabaseError as e:
